f4a4aa0ea38552bb3288107290583d33dd466af1
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-10 21:09:10 +0330

    Design Application Screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,36 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Vazir'),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange,
+          title: Text('سنگ   کاغذ    قیچی'),
+        ),
+        backgroundColor: Colors.deepOrange[600],
         body: SafeArea(
           child: Center(
-            child: Text('Initial App'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image(image: AssetImage('images/2.png')),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'شروع بازی',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Image(image: AssetImage('images/3.png')),
+              ],
+            ),
           ),
         ),
       ),
