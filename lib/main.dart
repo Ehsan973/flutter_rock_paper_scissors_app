6f2d3c2cad5051c16e3eb9e775ddd2935fd01284
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-10 20:51:45 +0330

    Initial Application

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('Initial App'),
+          ),
+        ),
+      ),
+    );
+  }
+}
